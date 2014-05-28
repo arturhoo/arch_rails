c977a47d4049cd54e8fe27247ba5f0b19d8f989f
Artur Rodrigues <arturhoo@gmail.com>
2014-05-28 14:19:52 -0300

    Broaden integration tests

--- spec/integration/repositories/customer_repository_spec.rb
+++ spec/integration/repositories/customer_repository_spec.rb
@@ -14,4 +14,15 @@ describe CustomerRepository do
       expect(customer.id).to eq(1)
     end
   end
+
+  describe 'adding a customer and retrieving it' do
+    before do
+      added_customer = repo.add(customer)
+      @found_customer = repo.find_by_id(added_customer.id)
+    end
+
+    it 'returns a model object with the same attributes' do
+      expect(@found_customer.attributes).to eq(customer.attributes)
+    end
+  end
 end
--- spec/integration/repositories/order_repository_spec.rb
+++ spec/integration/repositories/order_repository_spec.rb
@@ -4,7 +4,7 @@ describe OrderRepository do
   let(:repo) { OrderRepository.new }
   let(:order) { Order.new(value: 100, customer_id: 1) }
 
-  describe 'adding a order' do
+  describe 'adding an order' do
     it 'increments the number of order records' do
       expect { repo.add(order) }.to change { repo.all.size }.by(1)
     end
@@ -12,6 +12,17 @@ describe OrderRepository do
     it 'sets the order model instance id' do
       repo.add(order)
       expect(order.id).to eq(1)
+    end
+  end
+
+  describe 'adding an order and retrieving it' do
+    before do
+      added_order = repo.add(order)
+      @found_order = repo.find_by_id(added_order.id)
+    end
+
+    it 'returns a model object with the same attributes' do
+      expect(@found_order.attributes).to eq(order.attributes)
     end
   end
 end
